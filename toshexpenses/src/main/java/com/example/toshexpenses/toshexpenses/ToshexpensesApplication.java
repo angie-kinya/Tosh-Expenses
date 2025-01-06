5dadcf6e44a5429505d8aa188a227a8930e6ee11
angie-kinya <angiekinya670@gmail.com>
2025-01-06 21:27:03 +0300

    wip: Expense tracker spring boot application.
    Application beginning.

---
+++ toshexpenses/src/main/java/com/example/toshexpenses/toshexpenses/ToshexpensesApplication.java
@@ -0,0 +1,13 @@
+package com.example.toshexpenses.toshexpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToshexpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToshexpensesApplication.class, args);
+	}
+
+}
---
+++ toshexpenses/src/test/java/com/example/toshexpenses/toshexpenses/ToshexpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toshexpenses.toshexpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToshexpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
